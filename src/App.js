f48546bdbdf3c4826cc3618c8e9b4140f49d0b5b
OnealTristan <tristanjulian37@gmail.com>
2021-01-04 15:49:46 +0700

    Video 1-10

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+
+import Welcome from './components/Welcome'
+
+import Hello from './components/Hello'
+
+import Message from './components/Message'
+
+function App() {
+  return (
+    <div className="App">
+      <Message></Message>
+      {/* <Greet name="Bruce" heroName="Batman">
+        <p>This is Children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Woman"></Greet>
+      <Welcome name="Bruce" heroName="Batman"></Welcome>
+      <Welcome name="Clark" heroName="Superman"></Welcome>
+      <Welcome name="Diana" heroName="Wonder Woman"></Welcome> */}
+      {/* <Hello></Hello> */}
+    </div>
+  );
+}
+
+export default App;
